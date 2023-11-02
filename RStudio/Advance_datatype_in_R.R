67f53f7018130003989b80bdd5ad23af4649a8f4
Radha Raman Jha <radharamanjha1989@gmail.com>
2023-11-02 17:43:13 +0530

    R advanced Datatypes

--- RStudio/Advance_datatype_in_R.R
+++ RStudio/Advance_datatype_in_R.R
@@ -47,3 +47,212 @@ chv2
 typeof(chv2)
 intv2 = as.integer(chv2)
 typeof(intv2)
+
+# Vector Addition
+z <- c(1,2,3,4,5)
+z * 10
+z+10
+1/z
+
+
+# add two vector of equal length
+a = c(1,2,3,4)
+b = c(10,20,30,40)
+a+b
+
+# add two vector of unequal length
+d = c(1,2,3,4)
+e = c(10,20,30,40,50)
+d+e
+
+# length 
+length(d)
+
+# maximum
+max(d)
+
+# min
+min(d)
+
+# mean
+mean(d)
+
+# Sum
+sum(d)
+
+# Variance
+var(d)
+
+# Standard deviation
+sqrt(var(d))
+
+# sort
+sort(x)
+
+# reverse sort
+sort(x, decreasing = TRUE)
+
+# Access Data
+x
+x[3]
+x[2:4]
+
+# Negative value
+x[-2]
+x[-2:-4]
+
+# Boolean selection 
+x[x>6]
+
+# Is NA and is not NA
+test <- c(2,-1,NA,8,-9,NA)
+is.na(test)
+!is.na(test)
+
+# Combination of conditions
+test[!is.na(test) & test>0]
+
+# LIST  
+fruits <- list('Apple','Orange','Mango','Grapes','Papaya')
+typeof(fruits)
+lst <- list(name = 'Pat', wife ='Mary', child = 'Dolly', age = 23)
+lst
+lst$name
+lst$child
+lst$age
+
+# Conversion of list into vector vice a versa
+
+vec <- c(1,2,3,4)
+vlist = list(vec)
+typeof(vlist)
+newvec = unlist(vlist)
+typeof(newvec)
+fruits
+
+# Working with list
+
+fruits[3:5]
+fruits[[1]]
+newvec1 = unlist(lst)
+newvec1
+typeof(newvec1)
+
+for (X in newvec1){
+  print(X)
+}
+
+fruits
+fruits[6] = 'Kiwi'
+fruits
+fruits[6] = 'Pine'
+fruits
+
+# creation
+
+emp <- data.frame(
+  emp_id = c(1:5),
+  emp_name = c('tom','Dan','Ryan','Garty',"Pat"),
+  salary = c(1000,2300,1400,3700,4300),
+  start_date = as.Date(c('2010-01-09','2012-07-14','2011-08-18','2015-03-07','2013-12-30'))
+)
+emp
+
+# Setting location
+setwd('C:\\radha\\Data_visualiztion_files\\RStudio')
+udf <- read.csv('Universities.csv')
+udf
+
+emp.rnames <- data.frame(
+  emp_id = c(1:5),
+  emp_name = c('tom','Dan','Ryan','Garty',"Pat"),
+  salary = c(1000,2300,1400,3700,4300),
+  start_date = as.Date(c('2010-01-09','2012-07-14','2011-08-18','2015-03-07','2013-12-30')),
+  row.names = c('E1','E2','E3','E4','E5')
+)
+emp.rnames
+
+# Examine the Data
+str(emp)
+
+print(summary(emp))
+names(emp)
+rownames(emp)
+
+# head
+head(udf,3)
+
+# tail
+tail(udf,3)
+
+# Access Columns
+emp$emp_id
+emp$emp_name
+data.frame(emp$emp_name,emp$salary)
+
+emp[1:2]
+emp[,1:2]
+
+emp[,c(1,3)]
+
+# Access rows
+emp[1:2,]
+
+emp[c(1,3),]
+# can also be accessed by row name
+# emp['E1',1] -- check
+
+## Filter records based on condition
+emp[emp$emp_name == 'Ryan',]
+
+emp[emp$emp_name %in% c('Ryan','Dan','Pat')]
+
+udf[udf$Expenses > 40000 & udf$SAT > 1000,]
+
+# Add Column to DataFrame
+emp$dept = c('IT','Sales','Sales','HR','IT')
+emp
+
+
+emp2 <- data.frame(
+  emp_id = c(6:8),
+  emp_name = c('Harry','Charles','William'),
+  salary = c(1000,2300,1400),
+  start_date = as.Date(c('2010-01-09','2012-07-14','2011-08-18')),
+  dept= c('Sales','Operations','Operations')
+)
+emp.new = rbind(emp,emp2)
+emp.new
+
+# Change column name
+colnames(emp)
+colnames(emp)[2]<-'ename'
+emp
+
+# Change Entry
+emp$ename[emp$ename == 'Ryan'] <- 'Brayn'
+emp
+
+### Drop Columns and rows
+mydf = head(udf,10)
+mydf
+
+# Dropping rows
+
+mydf[-4,]
+mydf[-c(2,5,6),]
+mydf[-(2:6),]
+
+
+# Delete using row name
+# row.names(emp.rnames)
+
+
+# Delete Columns
+mydf[-4]
+mydf[-c(2,5,6)]
+mydf[-(2:6)]
+colnames(udf)
+udf[!(colnames(udf)%in% c('SAT'))]
+udf[!(colnames(udf)%in% c('SAT'))]
+
